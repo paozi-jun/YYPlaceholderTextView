abe54ab6648c04f4e5f9e157003e61289671afd7
向文品 <243888972@qq.com>
2014-06-26 12:45:23 +0800

    init

---
+++ YYPlaceholderTextView/YYPlaceholderTextView/MainViewController.swift
@@ -0,0 +1,37 @@
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var placeholderTextView = YYPlaceholderTextView(frame:CGRectMake(10, 50, 300, 200))
+        placeholderTextView.layer.borderWidth = 1
+        self.view.addSubview(placeholderTextView)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YYPlaceholderTextView/YYPlaceholderTextView/YYPlaceholderTextView.swift
@@ -0,0 +1,26 @@
+
+import UIKit
+
+class YYPlaceholderTextView: UITextView {
+
+    
+    var placeholder:NSString?
+    
+    var placeholderTextColor:UIColor?
+    
+    
+    init(frame: CGRect) {
+        super.init(frame: frame, textContainer:nil)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
